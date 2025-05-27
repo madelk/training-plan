fd3846bb36f6381d66a9292e63372e3bbe0dc83a
Mark Dell <madelk@gmail.com>
2025-05-27 20:17:19 +0100

    Delete apps/vuetest/.nuxt directory

--- apps/vuetest/.nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/markdell/code/vue/vuetest/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- apps/vuetest/.nuxt/dist/server/server.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///Users/markdell/code/vue/vuetest/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
