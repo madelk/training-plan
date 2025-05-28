a7a6f258eecd042b8a9230c114b5a112b7bd5e83
Mark Dell <madelk@gmail.com>
2025-05-28 10:54:42 +0100

    Display a name

--- libs/userName/src/pages/userName.vue
+++ libs/userName/src/pages/userName.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const userName = ref<string | undefined>();
+const submittedName = ref<string>("");
+const submitName = () => {
+  if (userName.value)
+    submittedName.value = userName.value;
+};
 </script>
 
 <template>
@@ -9,6 +14,11 @@ const userName = ref<string | undefined>();
     v-model="userName"
     placeholder="Enter your name"
   >
+  <button @click="submitName">
+    Submit
+  </button>
+  {{ submittedName }}
 </template>
 
 <style scoped></style>
+
