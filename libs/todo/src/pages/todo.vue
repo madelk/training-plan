9807b585507aa3f00baac8ed4262a9bc4fce9d70
Mark Dell <madelk@gmail.com>
2025-05-28 16:35:50 +0100

    feat: add user assignment to todo items and improve styling

--- libs/todo/src/pages/todo.vue
+++ libs/todo/src/pages/todo.vue
@@ -6,6 +6,20 @@ const fetcher = async () =>
     response.json(),
   );
 
+  const users = [
+    {
+      id: 1,
+      username: 'unicorn42',
+    },
+    {
+      id: 2,
+      username: 'rainbow88',
+    },
+    {
+      id: 3,
+      username: 'starrynight',
+    },
+  ]
 const { data, suspense, isLoading } = useQuery({ queryKey: ['todo'], queryFn: fetcher });
 
 await suspense();
@@ -18,14 +32,32 @@ await suspense();
     <p v-if="isLoading">
       Loading...
     </p>
-    <ul v-else>
+    <ul
+      v-else
+      style="list-style-type: none; padding: 0;"
+    >
       <li
         v-for="todo in data"
         :key="todo.id"
+        style="margin-bottom: 10px; padding: 10px; border: 1px solid #ccc; border-radius: 5px; margin-left: 8px"
       >
-        {{ todo.title }}
-        {{ todo.competed ? '(completed)' : '(not completed)' }}
-        {{ todo.userId }}
+        <input
+          :id="
+            `todo-checkbox-${todo.id}`"
+          style="vertical-align: middle;"
+          type="checkbox"
+          :checked="todo.completed"
+        >
+        <label
+          style="margin-left: 10px;"
+          :for="`todo-checkbox-${todo.id}`"
+        >
+          {{ todo.title }}
+        </label>
+        <span style="font-size: 0.8em; color: gray; margin-left: 10px;">
+          Assigned to:
+          {{ users.find(user => user.id === todo.userId)?.username || 'Unknown User' }}
+        </span>
       </li>
     </ul>
   </section>
